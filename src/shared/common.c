256c04c43b483d36624ac73325b3447a07057b25
Andrés Biso <biso.andres@gmail.com>
2024-10-26 16:31:18 -0300

    chore: refactor common to fit select()

--- src/shared/common.c
+++ src/shared/common.c
@@ -8,17 +8,8 @@
 #include <unistd.h>
 
 // Networking headers
-#include <arpa/inet.h>
-#include <netdb.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-
-// System headers
-#include <fcntl.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 // Other project headers
 #include "pack.h"
@@ -26,202 +17,62 @@
 // Project header
 #include "common.h"
 
-int malloc_string(char **s, int32_t size)
+char *allocate_string(const char *source)
 {
-    *s = (char *)malloc(size * sizeof(char));
-    if (*s == NULL)
-    {
-        puts("Error al asignar memoria");
-        perror("malloc");
-        return -1;
-    }
-    memset(*s, 0, size);
-    return 0;
-}
+    char *str;
+    size_t len;
 
-// use this function when you know the size the data
+    size_t len = strlen(source) + 1; // +1 for the null terminator
+    *str = (char *)malloc(len);
+    if (str == NULL)
+    {
+        fprintf(stderr, "Error al asignar memoria: %s\n", strerror(errno));
+        return NULL;
+    }
+    memset(str, 0, len); // Initialize allocated memory to zero
+    strcpy(str, source);
+    return str;
+}
 
 /* recvall:
  * Continuously receive data until the entire buffer is filled or the connection is closed.
  * We keep track of the total received data, and adjust the buffer pointer to avoid overwriting existing data.
- * `bytesleft` tracks how much of the buffer is yet to be filled, although `recv` manages the data reception amount.
+ * `bytes_left` tracks how much of the buffer is yet to be filled, although `recv` manages the data reception amount.
  */
-
-long recvall(int sockfd, char *buffer, int32_t buffer_size)
+ssize_t recvall(int sockfd, void *buf, size_t len)
 {
-    int32_t bytesleft, received, total_received;
-
-    total_received = 0;
-    bytesleft = buffer_size;
-
-    while (total_received < buffer_size)
-    {
-        received = recv(sockfd, buffer + total_received, bytesleft, 0);
-        if (received == -1)
-        {
-            free(buffer);
-            puts("Error al querer recibir datos");
-            perror("recv");
-            return -1;
-        }
-        else if (received == 0)
-        {
-            // received = 0: connection closed by the server
-            break;
-        }
-
-        total_received += received;
-    }
-
-    buffer[total_received] = '\0'; // Null-terminate the string
-    printf("Total recibido: %d bytes\n", total_received);
-    return total_received;
+    return recvall_with_flags(sockfd, buf, len, 0);
 }
 
-// use this function if you don't know the real size of the data
-long recvall_dynamic(int sockfd, char **buffer, int32_t *buffer_size)
+ssize_t recvall_with_flags(int sockfd, void *buf, size_t len, int flags)
 {
-    int32_t initial_size, total_received;
-    int32_t received;
+    char *buffer;
+    size_t bytes_left;
+    ssize_t total_received, received;
 
-    initial_size = *buffer_size;
+    *buffer = (char *)buf;
+    bytes_left = len;
     total_received = 0;
 
-    while (1)
+    while (total_received < len)
     {
-        received = recv(sockfd, *buffer + total_received, initial_size - total_received - 1, 0);
-        if (received == -1)
+        received = recv(sockfd, buffer + total_received, bytes_left, flags);
+        if (received < 0)
         {
-            free(*buffer);
-            puts("Error al querer recibir datos");
-            perror("recv");
-            return -1;
+            fprintf(stderr, "Error receiving data: %s\n", strerror(errno));
+            return received;
         }
         else if (received == 0)
         {
-            // received = 0: connection closed by the server
+            // Connection closed by the server
             break;
         }
-
         total_received += received;
-
-        if (total_received >= initial_size - 1)
-        {
-            initial_size *= 2; // Double the buffer size
-            if ((*buffer = (char *)realloc(*buffer, initial_size)) == NULL)
-            {
-                puts("Error al reasignar memoria");
-                perror("realloc");
-                return -1;
-            }
-            printf("Buffer incrementado de %d bytes a %d bytes\n", *buffer_size, initial_size);
-            *buffer_size = initial_size;
-        }
+        bytes_left -= received;
     }
 
-    (*buffer)[total_received] = '\0'; // Null-terminate the string
-    printf("Total recibido: %d bytes\n", total_received);
-    return total_received;
-}
-
-// recvall_dynamic and also prevents deadlocks
-long recvall_dynamic_timeout(int sockfd, char **buffer, int32_t *buffer_size)
-{
-    int flags, timeout;
-    int32_t initial_size, total_received;
-    int32_t received;
-
-    timeout = 1;
-    initial_size = *buffer_size;
-    total_received = 0;
-
-    struct timeval begin, now;
-    double timediff;
-
-    // Set socket to non-blocking mode
-    flags = fcntl(sockfd, F_GETFL, 0);
-    if (flags == -1)
-    {
-        perror("fcntl");
-        free(*buffer);
-        return -1;
-    }
-
-    if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1)
-    {
-        perror("fcntl");
-        free(*buffer);
-        return -1;
-    }
-
-    // beginning time
-    gettimeofday(&begin, NULL);
-
-    while (1)
-    {
-        gettimeofday(&now, NULL);
-
-        // time elapsed in seconds
-        timediff = (now.tv_sec - begin.tv_sec) + 1e-6 * (now.tv_usec - begin.tv_usec);
-
-        // if you got some data, then break after timeout
-        if (total_received > 0 && timediff > timeout)
-        {
-            break;
-        }
-        else if (timediff > timeout * 2)
-        {
-            // if you got no data at all, wait a little longer, twice the timeout
-            break;
-        }
-
-        received = recv(sockfd, *buffer + total_received, initial_size - total_received - 1, 0);
-        if (received == -1)
-        {
-            if (errno == EINTR || errno == EWOULDBLOCK || errno == EAGAIN)
-            {
-                continue; // if interrupted by signal, just continue
-            }
-            free(*buffer);
-            puts("Error al querer recibir datos");
-            perror("recv");
-            return -1;
-        }
-        else if (received == 0)
-        {
-            // received = 0: connection closed by the server
-            break;
-        }
-
-        total_received += received;
-
-        // reset beginning time
-        gettimeofday(&begin, NULL);
-
-        if (total_received >= initial_size - 1)
-        {
-            initial_size *= 2; // Double the buffer size
-            if ((*buffer = (char *)realloc(*buffer, initial_size)) == NULL)
-            {
-                puts("Error al reasignar memoria");
-                perror("realloc");
-                exit(EXIT_FAILURE);
-            }
-            printf("Buffer incrementado de %d bytes a %d bytes\n", *buffer_size, initial_size);
-            *buffer_size = initial_size;
-        }
-    }
-
-    (*buffer)[total_received] = '\0'; // Null-terminate the string
-    printf("Total recibido: %d bytes\n", total_received);
-
-    // Set socket back to blocking mode
-    if (fcntl(sockfd, F_SETFL, flags) == -1)
-    {
-        perror("fcntl");
-        free(*buffer);
-        return -1;
-    }
+    buffer[total_received] = '\0';                         // Null-terminate the string
+    printf("Total recibido: %zd bytes\n", total_received); // zd is used for printing variables of type ssize_t
     return total_received;
 }
 
@@ -231,21 +82,31 @@ long recvall_dynamic_timeout(int sockfd, char **buffer, int32_t *buffer_size)
  * We adjust the buffer pointer to the correct offset to avoid resending the same data.
  * The expression `length - total_sent` calculates the remaining amount of data to be sent.
  */
-
-long sendall(int sockfd, const char *buffer, int32_t length)
+ssize_t sendall(int sockfd, const void *buf, size_t len)
 {
-    int32_t total_sent = 0;
-    while (total_sent < length)
+    return sendall_with_flags(sockfd, buf, len, 0);
+}
+
+ssize_t sendall_with_flags(int sockfd, const void *buf, size_t len, int flags)
+{
+    const char *data;
+    size_t total_sent;
+    ssize_t bytes_sent;
+
+    data = (const char *)buf;
+    total_sent = 0;
+
+    while (total_sent < len)
     {
-        int32_t sent = send(sockfd, buffer + total_sent, length - total_sent, 0);
-        if (sent == -1)
+        bytes_sent = send(sockfd, data + total_sent, len - total_sent, flags);
+        if (bytes_sent < 0)
         {
-            puts("Error al querer enviar datos");
-            perror("send");
-            return -1;
+            fprintf(stderr, "Error al querer enviar datos: %s\n", strerror(errno));
+            return bytes_sent;
         }
-        total_sent += sent;
+        total_sent += bytes_sent;
     }
+
     printf("Total enviado: %d bytes\n", total_sent);
     return total_sent;
 }
@@ -255,19 +116,17 @@ int create_simple_packet(Simple_Packet **packet, const char *data)
     *packet = (Simple_Packet *)malloc(sizeof(Simple_Packet));
     if (*packet == NULL)
     {
-        puts("Error al asignar memoria");
-        perror("malloc");
+        fprintf(stderr, "Error al asignar memoria: %s\n", strerror(errno));
         return -1; // Memory allocation failed
     }
 
     (*packet)->length = strlen(data);
-    if (malloc_string(&((*packet)->data), (*packet)->length) != 0)
+    if (((*packet)->data = allocate_string(data)) == NULL)
     {
         free(*packet); // Free the struct if string allocation fails
         return -1;
     }
 
-    strcpy((*packet)->data, data);
     return 0; // Success
 }
 
@@ -277,120 +136,137 @@ int free_simple_packet(Simple_Packet *packet)
     {
         return -1; // Error: Packet is NULL
     }
-
     if (packet->data != NULL)
     {
         free(packet->data);
     }
     free(packet); // Free the packet struct itself
-
-    return 0; // Success
+    return 0;     // Success
 }
 
-int send_simple_packet(int sockfd, Simple_Packet *packet)
+ssize_t send_simple_packet(int sockfd, Simple_Packet *packet)
 {
     char *buffer;
     unsigned char *ubuffer;
-    int packetsize;
+    ssize_t sent_bytes, total_sentbytes;
 
-    malloc_string(&buffer, sizeof(int32_t));            // Allocate for int32_t length
-    ubuffer = (unsigned char *)malloc(sizeof(int32_t)); // Allocate for int32_t length
-    packetsize = pack(ubuffer, "l", packet->length);
+    total_sentbytes = 0;
 
-    printf("packet es de %d bytes\n", packetsize);
-    memcpy(buffer, ubuffer, sizeof(int32_t));
-
-    // Send the length first
-    if (sendall(sockfd, buffer, sizeof(int32_t)) == -1)
+    // Allocate memory for the length
+    buffer = (char *)malloc(sizeof(int32_t));
+    if (buffer == NULL)
     {
+        fprintf(stderr, "Error al asignar memoria: %s\n", strerror(errno));
+        return -1;
+    }
+
+    // Allocate memory for packing the length
+    ubuffer = (unsigned char *)malloc(sizeof(int32_t));
+    if (ubuffer == NULL)
+    {
+        fprintf(stderr, "Error al asignar memoria: %s\n", strerror(errno));
         free(buffer);
         return -1;
     }
 
-    // Send the data
-    if (sendall(sockfd, packet->data, packet->length) == -1)
+    if (pack(ubuffer, "l", packet->length) != sizeof(int32_t))
     {
+        fprintf(stderr, "Error al pack() el tamaño de la data\n");
+        free(buffer);
+        free(ubuffer);
         return -1;
     }
+
+    printf("Packet size: %d bytes\n", packet->length);
+    memcpy(buffer, ubuffer, sizeof(int32_t));
+
+    // Send the length first
+    if ((sent_bytes = sendall(sockfd, buffer, sizeof(int32_t))) < 0)
+    {
+        free(buffer);
+        free(ubuffer);
+        return sent_bytes;
+    }
+    total_sentbytes += sent_bytes;
+
+    // Send the data
+    if ((sent_bytes = sendall(sockfd, packet->data, packet->length)) < 0)
+    {
+        free(buffer);
+        free(ubuffer);
+        return sent_bytes;
+    }
+    total_sentbytes += sent_bytes;
 
     free(buffer);
     free(ubuffer);
-    return 0; // Success
+    return total_sentbytes;
 }
 
-int recv_simple_packet(int sockfd, Simple_Packet **packet)
+ssize_t recv_simple_packet(int sockfd, Simple_Packet **packet)
 {
     char *buffer;
-    int32_t *buffer_size;
     unsigned char *ubuffer;
+    ssize_t recv_bytes, ret_value, total_recvbytes;
 
-    malloc_string(&buffer, sizeof(int32_t));
-    ubuffer = (unsigned char *)malloc(sizeof(int32_t));
-    buffer_size = (int32_t *)malloc(sizeof(int32_t));
+    total_recvbytes = 0;
 
-    // Receive the length first
-    if (recvall(sockfd, buffer, sizeof(int32_t)) <= 0)
+    buffer = (char *)malloc(sizeof(int32_t));
+    if (buffer == NULL)
     {
+        fprintf(stderr, "Error al asignar memoria: %s\n", strerror(errno));
         return -1;
     }
 
-    // Create packet
-    if (create_simple_packet(packet, "") != 0)
+    ubuffer = (unsigned char *)malloc(sizeof(int32_t));
+    if (ubuffer == NULL)
     {
+        fprintf(stderr, "Error al asignar memoria: %s\n", strerror(errno));
+        free(buffer);
         return -1;
+    }
+
+    // Receive the length first
+    if ((recv_bytes = recvall(sockfd, buffer, sizeof(int32_t))) <= 0)
+    {
+        free(buffer);
+        free(ubuffer);
+        return recv_bytes;
+    }
+    total_recvbytes += recv_bytes;
+
+    // Create packet
+    if ((ret_value = (ssize_t)create_simple_packet(packet, "")) < 0)
+    {
+        free(buffer);
+        free(ubuffer);
+        return ret_value;
     }
 
     memcpy(ubuffer, buffer, sizeof(int32_t));
     unpack(ubuffer, "l", &((*packet)->length));
 
     // Allocate memory for the data
-    if (malloc_string(&((*packet)->data), (*packet)->length) != 0)
+    (*packet)->data = (char *)malloc((*packet)->length + 1); // +1 for the null terminator
+    if ((*packet)->data == NULL)
     {
-        free_simple_packet(*packet);
+        fprintf(stderr, "Error al asignar memoria: %s\n", strerror(errno));
+        free(buffer);
+        free(ubuffer);
         return -1;
     }
 
-    if (recvall(sockfd, (*packet)->data, (*packet)->length) <= 0)
+    if ((recv_bytes = recvall(sockfd, (*packet)->data, (*packet)->length)) <= 0)
     {
-        return -1;
+        free(buffer);
+        free(ubuffer);
+        return recv_bytes;
     }
+    total_recvbytes += recv_bytes;
+
     free(buffer);
-    free(buffer_size);
-    return 0; // Success
-}
-
-// ONLY USE IF USING FORK()
-// Setup signal handler for reaping zombie processes that appear as the fork()ed child processes exit.
-// This code ensures that terminated child processes are cleaned up immediately,
-// preventing resource leaks and keeping the process table uncluttered.
-int setup_signal_handler()
-{
-    struct sigaction sa;
-
-    sa.sa_handler = sigchld_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART; // Restart interrupted system calls
-    if (sigaction(SIGCHLD, &sa, NULL) == -1)
-    {
-        perror("sigaction");
-        return -1;
-    }
-    return 0;
-}
-
-// ONLY USE IF USING FORK()
-// Function to reap all dead processes
-void sigchld_handler(int s)
-{
-    (void)s; // Quiet unused variable warning
-
-    // waitpid() might overwrite errno, so we save and restore it:
-    int saved_errno = errno;
-
-    while (waitpid(-1, NULL, WNOHANG) > 0)
-        ;
-
-    errno = saved_errno;
+    free(ubuffer);
+    return total_recvbytes;
 }
 
 void simulate_work()
--- src/shared/common.h
+++ src/shared/common.h
@@ -14,17 +14,15 @@ typedef struct
     char *data;
 } Simple_Packet;
 
-int malloc_string(char **s, int32_t size);
-long recvall(int sockfd, char *buffer, int32_t buffer_size);
-long recvall_dynamic(int sockfd, char **buffer, int32_t *buffer_size);
-long recvall_dynamic_timeout(int sockfd, char **buffer, int32_t *buffer_size);
-long sendall(int sockfd, const char *buffer, int32_t length);
-int create_simple_packet(Simple_Packet **packet, char const *data);
+char *allocate_string(const char *source);
+ssize_t recvall(int sockfd, void *buf, size_t len);
+ssize_t recvall_with_flags(int sockfd, void *buf, size_t len, int flags);
+ssize_t sendall(int sockfd, const void *buf, size_t len);
+ssize_t sendall_with_flags(int sockfd, const void *buf, size_t len, int flags);
+int create_simple_packet(Simple_Packet **packet, const char *data);
 int free_simple_packet(Simple_Packet *packet);
-int send_simple_packet(int sockfd, Simple_Packet *packet);
-int recv_simple_packet(int sockfd, Simple_Packet **packet);
-int setup_signal_handler(void);
-void sigchld_handler(int s);
+ssize_t send_simple_packet(int sockfd, Simple_Packet *packet);
+ssize_t recv_simple_packet(int sockfd, Simple_Packet **packet);
 void simulate_work();
 
 #endif // COMMON_H
